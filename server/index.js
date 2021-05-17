ecaf4951bb422172ad851784eb84e1823989da13
unknown <nasrullah_hussaini@yahoo.com>
2021-05-17 15:18:16 -0700

    implement the nav

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"; 
 
 // Basic express setup:
 
@@ -20,7 +20,7 @@ const db = require("./lib/in-memory-db");
 //
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+const DataHelpers = require("./lib/data-helpers.js")(db);//???
 
 // Update the dates for the initial tweets (data-files/initial-tweets.json).
 require("./lib/date-adjust")();
