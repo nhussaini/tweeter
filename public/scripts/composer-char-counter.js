4e41b20e7e2a9ebaf7df80a950e8ec432c5cf4a4
unknown <nasrullah_hussaini@yahoo.com>
2021-05-18 13:07:29 -0700

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  const maxLength = 140;
+  $('textarea').on('keyup', function(event) {
+    //get the text length from textarea
+    let textLen = maxLength - ($(this).val().length);
+    // add class red if textlength < 0
+    if(textLen < 0) {
+      $(this).parents("form").children("div.new-tweet-submit").children(".counter").addClass("red");  
+      // remove class red if the textlength > 0  
+    } else {
+      $(this).parents("form").children("div.new-tweet-submit").children(".counter").removeClass("red");
+    }
+    //dynnamically reduce the count of the characters
+    $(this).parents("form").children("div.new-tweet-submit").children(".counter").text(textLen)
+  });
+});
